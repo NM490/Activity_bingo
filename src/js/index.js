75a8667f91312d41b13f7a9ccd418a35798c7078
NM490 <nminato490@gmail.com>
2024-03-18 08:45:30 +0800

    modify javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,37 @@
-let B = false,
-    I = false,
-    N = false,
-    G = false,
-    O = false;
+let isB = false,
+    isI = false,
+    isN = false,
+    isG = false,
+    isO = false;
+
+let B = [], I = [], N = [], G = [], O = [];
 
 function newNumber() {
     let x = Math.floor(Math.random() * 75) + 1;
     if (x <= 15) {
         generateNewNumber(15);
-        B = true;
+        isB = true;
     } else if (x >= 16 && x <= 30) {
         generateNewNumber(30);
-        I = true;
+        isI = true;
     }
     else if (x >= 31 && x <= 45) {
         generateNewNumber(45);
-        N = true;
+        isN = true;
     }
     else if (x >= 46 && x <= 60) {
         generateNewNumber(60);
-        G = true;
+        isG = true;
     }
     else if (x >= 61 && x <= 75) {
         generateNewNumber(75);
-        O = true;
+        isO = true;
     }
 
-    if (B == true && I == true && N == true && G == true && O == true) {
+    if (isB == true && isI == true && isN == true && isG == true && isO == true) {
         console.log("BINGO!!!!!!")
+        console.table({B,I,N,G,O})
     }
-
 }
 
 function generateNewNumber(maxNumber) {
@@ -37,20 +39,43 @@ function generateNewNumber(maxNumber) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(maxNumber);
 
-    let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-
     switch (maxNumber) {
-        case 15:
+        case 15: {
+            let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+            B.push(x);
             return console.log(`B: ${x}`)
-        case 30:
+        }
+        case 30: {
+            let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+            I.push(x);
             return console.log(`I: ${x}`)
-        case 45:
+        }
+        case 45: {
+            let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+            N.push(x);
             return console.log(`N: ${x}`)
-        case 60:
+        }
+        case 60: {
+            let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+            G.push(x);
             return console.log(`G: ${x}`)
-        case 75:
+        }
+        case 75: {
+            let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+            O.push(x);
             return console.log(`O: ${x}`)
+        }
     }
 
 }
-console.log(newNumber());
+/*
+for (let i = 0; i < 100; i++) {
+    newNumber();
+    if (isB == true && isI == true && isN == true && isG == true && isO == true) {
+        break;
+    }
+}*/
+
+while (!isB || !isI || !isN || !isG || !isO ) {
+    newNumber();
+}
