115ccab5d9aa6755a7609c0ed41d81704c7a0b9d
NM490 <nminato490@gmail.com>
2024-03-20 08:27:04 +0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,81 +1,96 @@
-let isB = false,
-    isI = false,
-    isN = false,
-    isG = false,
-    isO = false;
+let bingoData = [[], [], [], [], []];
+const bingoBody = document.getElementById("bingoBody");
 
-let B = [], I = [], N = [], G = [], O = [];
+generateNumber = (maxNumber) => {
+  const min = maxNumber - 15 + 1;
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(maxNumber);
+  let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+  return x;
+};
 
-function newNumber() {
-    let x = Math.floor(Math.random() * 75) + 1;
-    if (x <= 15) {
-        generateNewNumber(15);
-        isB = true;
-    } else if (x >= 16 && x <= 30) {
-        generateNewNumber(30);
-        isI = true;
-    }
-    else if (x >= 31 && x <= 45) {
-        generateNewNumber(45);
-        isN = true;
-    }
-    else if (x >= 46 && x <= 60) {
-        generateNewNumber(60);
-        isG = true;
-    }
-    else if (x >= 61 && x <= 75) {
-        generateNewNumber(75);
-        isO = true;
-    }
-
-    if (isB == true && isI == true && isN == true && isG == true && isO == true) {
-        console.log("BINGO!!!!!!")
-        console.table({B,I,N,G,O})
-    }
-}
-
-function generateNewNumber(maxNumber) {
-    const min = (maxNumber - 15) + 1;
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(maxNumber);
-
-    switch (maxNumber) {
-        case 15: {
-            let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-            B.push(x);
-            return console.log(`B: ${x}`)
+generateBingoArray = (bingoArray) => {
+  switch (bingoArray) {
+    case "B": {
+      let loop = true;
+      while (loop) {
+        let x = generateNumber(15);
+        if (!bingoData[0].includes(x)) {
+          bingoData[0].push(x);
+          loop = false;
         }
-        case 30: {
-            let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-            I.push(x);
-            return console.log(`I: ${x}`)
+      }
+      break;
+    }
+    case "I": {
+      let loop = true;
+      while (loop) {
+        let x = generateNumber(30);
+        if (!bingoData[1].includes(x)) {
+          bingoData[1].push(x);
+          loop = false;
         }
-        case 45: {
-            let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-            N.push(x);
-            return console.log(`N: ${x}`)
+      }
+      break;
+    }
+    case "N": {
+      let loop = true;
+      while (loop) {
+        let x = generateNumber(45);
+        if (!bingoData[2].includes(x)) {
+          bingoData[2].push(x);
+          loop = false;
         }
-        case 60: {
-            let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-            G.push(x);
-            return console.log(`G: ${x}`)
+      }
+      break;
+    }
+    case "G": {
+      let loop = true;
+      while (loop) {
+        let x = generateNumber(60);
+        if (!bingoData[3].includes(x)) {
+          bingoData[3].push(x);
+          loop = false;
         }
-        case 75: {
-            let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-            O.push(x);
-            return console.log(`O: ${x}`)
+      }
+      break;
+    }
+    case "O": {
+      let loop = true;
+      while (loop) {
+        let x = generateNumber(75);
+        if (!bingoData[4].includes(x)) {
+          bingoData[4].push(x);
+          loop = false;
         }
+      }
+      break;
+    }
+  }
+};
+
+populateBingoTable = () => {
+  //GENERATE NUMBERS
+  while (bingoData.some((row) => row.length !== 5)) {
+    generateBingoArray("B");
+    generateBingoArray("I");
+    generateBingoArray("N");
+    generateBingoArray("G");
+    generateBingoArray("O");
+  }
+
+  //ROW
+  for (let i = 0; i < bingoData[0].length; i++) {
+    const tableRow = document.createElement("tr");
+    //COLUMN
+    for (let j = 0; j < bingoData.length; j++) {
+      const tableCell = document.createElement("td");
+      tableCell.textContent = bingoData[j][i];
+      tableRow.appendChild(tableCell);
     }
 
-}
-/*
-for (let i = 0; i < 100; i++) {
-    newNumber();
-    if (isB == true && isI == true && isN == true && isG == true && isO == true) {
-        break;
-    }
-}*/
+    bingoBody.appendChild(tableRow);
+  }
+};
 
-while (!isB || !isI || !isN || !isG || !isO ) {
-    newNumber();
-}
+populateBingoTable();
